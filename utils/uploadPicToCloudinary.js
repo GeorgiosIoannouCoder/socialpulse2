02b0ce952e2e5a973778f60bab3a48a9c22287e3
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2024-02-20 08:17:47 -0500

    Update utils/uploadPicToCloudinary.js

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,7 +4,7 @@ const uploadPic = async (media) => {
   try {
     const form = new FormData();
     form.append("file", media);
-    form.append("upload_preset", "socialpulse"); // Replace socialpulse with your own cloudinary upload preset.
+    form.append("upload_preset", "socialpulse2"); // Replace socialpulse2 with your own cloudinary upload preset.
     form.append("cloud_name", "dgnigx1ez"); // Replace dgnigx1ez with your own cludinary cloud name.
 
     const res = await axios.post(
