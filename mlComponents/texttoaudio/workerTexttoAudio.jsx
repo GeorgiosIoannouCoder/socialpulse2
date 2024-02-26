081da076e0aae4d6de4eebed70a0dc01a8207bf1
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2024-02-26 04:36:16 -0500

    Update mlComponents/texttoaudio/workerTexttoAudio.jsx

--- mlComponents/texttoaudio/workerTexttoAudio.jsx
+++ mlComponents/texttoaudio/workerTexttoAudio.jsx
@@ -1,32 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const playAudio = async ( text ) => {
-  
-  const voiceId = '21m00Tcm4TlvDq8ikWAM';
+const playAudio = async (text) => {
+  const voiceId = "21m00Tcm4TlvDq8ikWAM";
 
   const apiRequestOptions = {
-    method: 'POST',
+    method: "POST",
     url: `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
     headers: {
-      accept: 'audio/mpeg',
-      'content-type': 'application/json',
-      'xi-api-key': 'e4f62192ead2c018d1fb93f37d79809b',
+      accept: "audio/mpeg",
+      "content-type": "application/json",
+      "xi-api-key": "e4f62192ead2c018d1fb93f37d79809b",
     },
     data: {
       text: text,
     },
-    responseType: 'blob', // To receive binary data in response
+    responseType: "blob", // To receive binary data in response
   };
 
   const apiResponse = await axios.request(apiRequestOptions);
 
-
-  console.log("hello")
+  // console.log("hello")
   const audio = new Audio(URL.createObjectURL(apiResponse.data));
   audio.play();
 
-  await new Promise(resolve => {
-    audio.addEventListener('ended', () => {
+  await new Promise((resolve) => {
+    audio.addEventListener("ended", () => {
       resolve();
     });
   });
